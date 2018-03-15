236c35e82856b87bc38eecb9dbb244e13f46015a
craftwar <craftwar@users.noreply.github.com>
2018-03-15 15:48:18 +0800

    Enable Compiler Intrinsics

--- script/ffmpeg_compile.sh
+++ script/ffmpeg_compile.sh
@@ -1,6 +1,7 @@
 cd /c/projects/$APPVEYOR_PROJECT_NAME/ffmpeg
 PKG_CONFIG_PATH="$pkg_prefix_mingw/lib/pkgconfig" LDFLAGS="-L/home$pkg_prefix_mingw/lib" CPPFLAGS="-I$pkg_prefix_mingw/include" \
 	./configure --prefix=$pkg_prefix_mingw --enable-shared \
+	--extra-cflags='-Oi' --extra-cflags='-Oi' \
 	--extra-cflags="-I$pkg_prefix_win\include" --extra-ldflags="/LIBPATH:$pkg_prefix_win\lib" \
 	--toolchain=msvc --enable-version3 --enable-gpl --disable-programs --disable-doc --arch=x86_64 --enable-libx264 \
 	--enable-zlib --enable-encoder=png \
