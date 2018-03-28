f647d84b7a91d63e2726133969325aaa77b921a4
craftwar <craftwar@users.noreply.github.com>
2018-03-28 21:19:48 +0800

    Add -MP (Build with Multiple Processes) compiler option, make /j (build faster?)

--- script/ffmpeg_compile.sh
+++ script/ffmpeg_compile.sh
@@ -1,11 +1,11 @@
 cd /c/projects/$APPVEYOR_PROJECT_NAME/ffmpeg
 PKG_CONFIG_PATH="$pkg_prefix_mingw/lib/pkgconfig" LDFLAGS="-L/home$pkg_prefix_mingw/lib" CPPFLAGS="-I$pkg_prefix_mingw/include" \
 	./configure --prefix=$pkg_prefix_mingw --enable-shared \
-	--extra-cflags='-Oi' --extra-cflags='-Oi' \
-	--extra-cflags="-I$pkg_prefix_win\include" --extra-ldflags="-LIBPATH:$pkg_prefix_win\lib" \
+	--extra-cxxflags='-MP -Oi -arch:AVX2' \
+	--extra-cflags="-MP -Oi -I$pkg_prefix_win\include" --extra-ldflags="-LIBPATH:$pkg_prefix_win\lib" \
 	--toolchain=msvc --enable-version3 --enable-gpl --disable-programs --disable-doc --arch=x86_64 --enable-libx264 \
 	--enable-zlib --enable-encoder=png \
 	--disable-xlib \
 	--disable-postproc --disable-debug
-make
+make /j
 make install
