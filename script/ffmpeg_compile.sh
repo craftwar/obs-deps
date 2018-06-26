ce49131d4596bd56d96981685f7a05e57bbfb787
craftwar <craftwar@users.noreply.github.com>
2018-06-27 00:53:47 +0800

    ffmpeg: Can't use /GL or arch:AVX2

--- script/ffmpeg_compile.sh
+++ script/ffmpeg_compile.sh
@@ -2,7 +2,7 @@ cd /c/projects/$APPVEYOR_PROJECT_NAME/ffmpeg
 date
 PKG_CONFIG_PATH="$pkg_prefix_mingw/lib/pkgconfig" LDFLAGS="-L/home$pkg_prefix_mingw/lib" CPPFLAGS="-I$pkg_prefix_mingw/include" \
 	./configure --prefix=$pkg_prefix_mingw --enable-shared \
-	--extra-cflags="-MP -Oi -arch:AVX2 -GL -DHAVE_UNISTD_H=0 -I$pkg_prefix_win\include" \
+	--extra-cflags="-MP -Oi -DHAVE_UNISTD_H=0 -I$pkg_prefix_win\include" \
 	--extra-cxxflags="-MP -Oi -arch:AVX2 -GL -I$pkg_prefix_win\include" \
 	--extra-ldflags="-LTCG -LIBPATH:$pkg_prefix_win\lib" \
 	--toolchain=msvc --enable-version3 --enable-gpl --disable-programs --disable-doc --arch=x86_64 --host-os=win64 --target-os=win64 --cpu=i686\
