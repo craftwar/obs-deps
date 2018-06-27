078242f0c020de3604b6a4a1d1ed94f46894bc32
craftwar <craftwar@users.noreply.github.com>
2018-06-27 09:44:59 +0800

    Add /LTCG linker option explicitly (may be better, no linker restart?)

--- script/ffmpeg_compile.sh
+++ script/ffmpeg_compile.sh
@@ -3,7 +3,7 @@ PKG_CONFIG_PATH="$pkg_prefix_mingw/lib/pkgconfig" LDFLAGS="-L/home$pkg_prefix_mi
 	./configure --prefix=$pkg_prefix_mingw --enable-shared \
 	--extra-cflags="-MP -Oi -I$pkg_prefix_win\include" \
 	--extra-cxxflags="-MP -Oi -arch:AVX2 -GL -I$pkg_prefix_win\include" \
-	--extra-ldflags="-LIBPATH:$pkg_prefix_win\lib" \
+	--extra-ldflags="-LTCG -LIBPATH:$pkg_prefix_win\lib" \
 	--toolchain=msvc --enable-version3 --enable-gpl --disable-programs --disable-doc --arch=x86_64 --enable-libx264 \
 	--enable-zlib --enable-encoder=png \
 	--disable-xlib \
