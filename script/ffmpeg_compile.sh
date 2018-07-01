56692f1eb21668d9eabba668c07aea3b06206813
craftwar <craftwar@users.noreply.github.com>
2018-07-01 10:32:03 +0800

    Add another matrix /favor:AMD64 compiler option

--- script/ffmpeg_compile.sh
+++ script/ffmpeg_compile.sh
@@ -2,8 +2,8 @@ cd /c/projects/$APPVEYOR_PROJECT_NAME/ffmpeg
 date
 PKG_CONFIG_PATH="$pkg_prefix_mingw/lib/pkgconfig" LDFLAGS="-L/home$pkg_prefix_mingw/lib" CPPFLAGS="-I$pkg_prefix_mingw/include" \
 	./configure --prefix=$pkg_prefix_mingw --enable-shared \
-	--extra-cflags="-MP -Oi -Gw -Gy -I$pkg_prefix_win\include" \
-	--extra-cxxflags="-MP -Oi -arch:AVX2 -Gw -Gy -GL -I$pkg_prefix_win\include" \
+	--extra-cflags="-MP -Oi -Gw -Gy -favor:$favor_arch -I$pkg_prefix_win\include" \
+	--extra-cxxflags="-MP -Oi -arch:AVX2 -Gw -Gy -GL -favor:$favor_arch -I$pkg_prefix_win\include" \
 	--extra-ldflags="-LTCG -LIBPATH:$pkg_prefix_win\lib" \
 	--toolchain=msvc --enable-version3 --enable-gpl --disable-programs --disable-doc --arch=x86_64 --host-os=win64 --target-os=win64 --cpu=i686\
 	--enable-libx264 \
