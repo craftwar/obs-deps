e42dac60e383581bc28743af6018583080567b15
craftwar <craftwar@users.noreply.github.com>
2018-09-18 09:40:09 +0800

    .h

--- script/ffmpeg_patch_pl.sh
+++ script/ffmpeg_patch_pl.sh
@@ -1,6 +1,6 @@
 cd /c/projects/$APPVEYOR_PROJECT_NAME/ffmpeg
 find . -type f -name '*.c' -type f -print0 | xargs -0 perl -0777 -pi -e 's/(else )?if \(ARCH_(?!X86).+?\)(.|\n)+?;//mng'
-find . -type f -name '*.h' -type f -print0 | xargs -0 perl -0777 -pi -e 's/.+?_(aarch64|arm|ppc)\(.|\n+?\);//mng'
+find . -type f -name '*.h' -type f -print0 | xargs -0 perl -0777 -pi -e 's/.+?_(aarch64|arm|ppc)\((.|\n)+?\);//mng'
 
 # ?! negative lookadhead
 # /mng, muti-lines/ global
