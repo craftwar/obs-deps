fdde78504ced7e45f303e9c92b440fb131eda374
craftwar <craftwar@users.noreply.github.com>
2018-09-18 11:13:38 +0800

    x86_32

--- script/ffmpeg_patch_pl.sh
+++ script/ffmpeg_patch_pl.sh
@@ -1,7 +1,9 @@
 cd /c/projects/$APPVEYOR_PROJECT_NAME/ffmpeg
 find . -type f -name '*.c' -type f -print0 | xargs -0 perl -0777 -pi -e 's/(else )?if\s*?\(ARCH_(?!X86).+?\)(.|\n)+?;//mng'
-find . -type f -name '*.h' -type f -print0 | xargs -0 perl -0777 -pi -e 's/.+?_(aarch64|arm|ppc)\((.|\n)+?\);//mng'
+find . -type f -name '*.c' -type f -print0 | xargs -0 perl -0777 -pi -e 's/(else )?if\s*?\(ARCH_X86_32.+?\)\s*?{(.|\n)*?}|\s*?(.|\n)*?;(?!\s*?({|}))//mng'
 
+# ARCH_X86_32
+#find . -type f -name '*.h' -type f -print0 | xargs -0 perl -0777 -pi -e 's/.+?_(aarch64|arm|ppc)\((.|\n)+?\);//mng'
 # ?! negative lookadhead
 # /mng, muti-lines/ global
 # use /n no capture group, (?:) is not required
