27ddbe26cd8ca886bca3a1d01267c811e6e167f2
craftwar <craftwar@users.noreply.github.com>
2019-05-24 19:38:34 +0800

    CI: Upgrade to VS2019

--- script/set_variable.cmd
+++ script/set_variable.cmd
@@ -3,7 +3,7 @@
 @rem both = C:\projects\x264
 @rem use APPVEYOR_PROJECT_NAME
 
-set zlib_Version=1.2.11
-set libpng_Version=1.6.32
+@rem set zlib_Version=1.2.11
+@rem set libpng_Version=1.6.32
 set pkg_prefix_win=%APPVEYOR_BUILD_FOLDER%\packages\win64
 set pkg_prefix_mingw=/c/projects/%APPVEYOR_PROJECT_NAME%/packages/win64
