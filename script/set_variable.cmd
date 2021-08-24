52f6022e614d4e83ae45ec2d09dd615cf0a18ff4
craftwar <craftwar@users.noreply.github.com>
2021-08-24 18:04:41 +0800

    Add 32bit Detours build scripts

--- script/set_variable.cmd
+++ script/set_variable.cmd
@@ -5,5 +5,6 @@
 
 @rem set zlib_Version=1.2.11
 @rem set libpng_Version=1.6.32
+set pkg_prefix=%APPVEYOR_BUILD_FOLDER%\packages
 set pkg_prefix_win=%APPVEYOR_BUILD_FOLDER%\packages\win64
 set pkg_prefix_mingw=/c/projects/%APPVEYOR_PROJECT_NAME%/packages/win64
